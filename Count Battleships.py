8b94fdc17e80889e9bd86b3c2efec33815a71e1e
Antonio Scaramazza <tonyaleja@gmail.com>
2017-05-19 19:47:16 -0400

    Update Count Battleships.py

--- Count Battleships.py
+++ Count Battleships.py
@@ -1,4 +1,4 @@
-Given an 2D board, count how many battleships are in it. The battleships are represented with 'X's, empty slots are represented with '.'s. You may assume the following rules:
+# Given an 2D board, count how many battleships are in it. The battleships are represented with 'X's, empty slots are represented with '.'s. You may assume the following rules:
 
 # You receive a valid board, made of only battleships or empty slots.
 # Battleships can only be placed horizontally or vertically. In other words, they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
